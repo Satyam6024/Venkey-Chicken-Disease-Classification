c228cc741c8254fa9c820cb1ab428587f9371987
Satyam Shivam Sahu <sss.sahusatyam2000@gmail.com>
2025-08-15 17:22:11 +0530

    train models

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from ChickenDiseaseClassifier import logger
 from ChickenDiseaseClassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from ChickenDiseaseClassifier.pipeline.stage02_base_model import PrepareBaseModelTrainingPipeline
+from ChickenDiseaseClassifier.pipeline.stage03_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -21,6 +23,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
